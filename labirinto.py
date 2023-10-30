70dc376873bff583eb444545c6f8c0572245cf62
kennedywc <kennedy.costan10@gmail.com>
2023-10-30 13:05:27 -0300

    base para o pygame

--- labirinto.py
+++ labirinto.py
@@ -35,3 +35,7 @@ if __name__ == '__main__':
     labirinto = Labirinto()
     labirinto.carregar_labirinto()
     labirinto.exibir()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,38 @@
+# Local
+from constantes import *
+
+# PYGAME
+import pygame
+from sys import exit
+
+class Game:
+    def __init__(self):
+
+        # Inicialize o Pygame
+        pygame.init()
+
+        # Crie uma tela Pygame
+        self.__tela = pygame.display.set_mode((LARGURA, ALTURA))
+        pygame.display.set_caption(TITULO_JANELA)
+
+    def iniciar(self):
+
+        # Loop principal do jogo
+        while True:
+
+            self.__tela.fill(COR_PRETA)
+
+            # capturando eventos da janela
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    # Encerra o Pygame
+                    pygame.quit()
+                    exit()
+
+            # Atualize a tela dentro do loop
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.iniciar()
